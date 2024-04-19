2f8169fd1aa21c81a622e7db502f82c573e51b00
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-04-19 12:26:20 -0300

    Declare new variable correctly

--- sausage-script.js
+++ sausage-script.js
@@ -1,6 +1,6 @@
 // As a boolean; always start stowed
 Config.ui.stowBarInitially = true;
 
-foreground = document.createElement("div");
+var foreground = document.createElement("div");
 foreground.id = "foreground";
 document.body.appendChild(foreground);
