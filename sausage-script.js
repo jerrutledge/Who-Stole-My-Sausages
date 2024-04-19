b9c3ea67b33a40e50b912eea6f5d535e8670a64c
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-04-19 12:24:33 -0300

    Add custom foreground and background styles

--- sausage-script.js
+++ sausage-script.js
@@ -1,2 +1,6 @@
 // As a boolean; always start stowed
 Config.ui.stowBarInitially = true;
+
+foreground = document.createElement("div");
+foreground.id = "foreground";
+document.body.appendChild(foreground);
