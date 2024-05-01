2e40980bfb7494a16e15a6974b11049a6158c55b
Jeremy Rutledge <jerrutledge@users.noreply.github.com>
2024-04-30 22:04:17 -0300

    refactor sausage script file

--- sausage-script.js
+++ sausage-script.js
@@ -1,15 +1,3 @@
-// Add setting for profanity
-Setting.addToggle("Profanity", definition)
-var settingProfanityToggle = function () {
-    var setProfanity = settings.profanity;
-    State.setVar('$profanity', setProfanity);
-};
-Setting.addToggle("profanity", {
-	label    : "Profanity",
-	default  : true,
-	onChange : settingProfanityToggle
-});
-
 // As a boolean; always start stowed
 Config.ui.stowBarInitially = true;
 
@@ -35,30 +23,6 @@ $(document).on(':passagerender', function(ev){
 	
 });
 
-
-window.on_click_settings = function() {
-    Dialog.setup();
-    var content = document.createElement('div');
-
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = 'profanity_checkbox';
-    checkbox.checked = State.getVar('$profanity');
-    checkbox.addEventListener('change', (event) => {
-        State.setVar('$profanity', event.currentTarget.checked);
-    });
-    content.appendChild(checkbox);
-
-    var label = document.createElement('label');
-    label.setAttribute('for', 'profanity_checkbox');
-    label.innerHTML = "Profanity";
-    content.appendChild(label);
-
-    Dialog.append(content);
-    Dialog.open();
-
-    return false;
-}
 
 
 function preloader() {
@@ -87,3 +51,15 @@ function addLoadEvent(func) {
 	}
 }
 addLoadEvent(preloader);
+
+// Add setting for profanity
+Setting.addToggle("Profanity", definition)
+var settingProfanityToggle = function () {
+    var setProfanity = settings.profanity;
+    State.setVar('$profanity', setProfanity);
+};
+Setting.addToggle("profanity", {
+	label    : "Profanity",
+	default  : true,
+	onChange : settingProfanityToggle
+});
